54518a7e334d39e2a2c9e2e0ad393356aa0887ec
j-a-sunny <md.j.a.sunny@outlook.com>
2025-09-28 21:03:02 +0600

    Removed video lazy loading

--- app/components/Video.jsx
+++ app/components/Video.jsx
@@ -7,8 +7,7 @@ const Video = () => {
       src="https://www.youtube.com/embed/77A0iIGT3RQ?autoplay=1&mute=1&loop=1&playlist=77A0iIGT3RQ&controls=0&si=dZ7MfrgdvT4Bcjpy"
       allow="autoplay"
       allowFullScreen
-      className='  w-full aspect-video'
-      loading="lazy"
+      className='w-full aspect-video'
     />
     </div>
   )
