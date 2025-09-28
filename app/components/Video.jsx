7650e9289c1552ee0add724a1ffbc26e53e55f2f
j-a-sunny <md.j.a.sunny@outlook.com>
2025-09-28 20:50:30 +0600

    Created and finished video part

---
+++ app/components/Video.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div className="container py-8">
+      <iframe
+      src="https://www.youtube.com/embed/77A0iIGT3RQ?autoplay=1&mute=1&loop=1&playlist=77A0iIGT3RQ&controls=0&si=dZ7MfrgdvT4Bcjpy"
+      allow="autoplay"
+      allowFullScreen
+      className='  w-full aspect-video'
+      loading="lazy"
+    />
+    </div>
+  )
+}
+
+export default Video
