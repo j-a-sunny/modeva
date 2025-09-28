224f093828a5a8ad9cd8fac6e2e65426d44236b6
j-a-sunny <md.j.a.sunny@outlook.com>
2025-09-28 22:37:21 +0600

    made outfit section and single product component more dynamic

--- app/components/OutfitWomen.jsx
+++ app/components/OutfitWomen.jsx
@@ -5,22 +5,24 @@ import SingleProduct from './SingleProduct'
 
 const OutfitWomen = () => {
   return (
-    <div className='container flex flex-col py-8 gap-y-8'>
+    <div className='container flex flex-col py-8 px-4 gap-y-8'>
       <div>
         <h2 className='font-prime font-medium text-[36px] md:text-[56px] lg:text-[76px] leading-tight md:leading-[1.1] uppercase'>the best dress for the best woman</h2>
       </div>
 
-      {/* product carasoul div */}
-      <div className="product grid gap-y-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 ">
+      {/* product carasoul div - Updated Grid */}
+      <div className="product grid gap-x-4 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 
         {/* single product */}
         <SingleProduct />
         <SingleProduct />
         <SingleProduct />
         <SingleProduct />
+        <SingleProduct /> {/* Added a couple more for better visual testing */}
+        <SingleProduct />
 
       </div>
-      <div className="">
+      <div className="flex justify-center"> {/* Added flex justify-center to center the button */}
         <button className="inline-flex items-center justify-center px-8 py-4 font-second font-semibold text-[16px] text-white bg-primary uppercase">
           see more<HiOutlineArrowSmallRight />
         </button>
