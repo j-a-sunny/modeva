8d42a6c79e6a887be5329173505f823b83cac86c
j-a-sunny <md.j.a.sunny@outlook.com>
2025-10-19 19:44:14 +0600

    Made the bento part responsive on mobile screens

--- app/components/Bento.jsx
+++ app/components/Bento.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Bento = () => {
   return (
-    <div className='flex container gap-8 py-8 font-prime font-medium text-5xl text-white uppercase'>
+    <div className='flex flex-col sm:flex-row container gap-8 py-8 font-prime font-medium text-[36px] lg:text-5xl text-white uppercase'>
       {/* formal */}
-      <div className="flex w-1/2 flex-col gap-y-8">
-        <div className="flex items-center w-full h-[374] bg-[url('/formal-woman.png')] bg-cover bg-center p-[30px] ">formal woman</div>
-        <div className="flex items-center w-full h-[374] bg-[url('/formal-man.png')] bg-cover bg-center p-[30px] ">formal man</div>
+      <div className="flex sm:w-1/2 w-full flex-col gap-y-8">
+        <div className="flex items-end justify-baseline w-full h-[374] bg-[url('/formal-woman.png')] bg-cover bg-center p-[30px] ">formal woman</div>
+        <div className="flex items-end justify-baseline w-full h-[374] bg-[url('/formal-man.png')] bg-cover bg-center p-[30px] ">formal man</div>
       </div>
       {/* casual */}
-      <div className="flex items-center w-1/2 h-[780px] bg-[url('/casual.png')] bg-cover bg-center p-[55px]">casual style</div>
+      <div className="flex items-center sm:w-1/2 w-full h-[780px] bg-[url('/casual.png')] bg-cover bg-center p-[55px]">casual style</div>
     </div>
   )
 }
